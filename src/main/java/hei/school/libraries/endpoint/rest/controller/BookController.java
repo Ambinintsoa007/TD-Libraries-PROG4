ef243ecba2c4934bfd2f65b03f5b785a3da593be
narindra20 <hei.narindra@gmail.com>
2026-06-09 12:24:37 +0300

    feat(controller): add BookController

---
+++ src/main/java/hei/school/libraries/endpoint/rest/controller/BookController.java
@@ -0,0 +1,40 @@
+package hei.school.libraries.endpoint.rest.controller;
+
+import hei.school.libraries.entity.Book;
+import hei.school.libraries.service.BookService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/books")
+@RequiredArgsConstructor
+public class BookController {
+
+    private final BookService bookService;
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @GetMapping("/{id}")
+    public Book getBookById(@PathVariable String id) {
+        return bookService.getBookById(id);
+    }
+
+    @PostMapping
+    public Book createBook(@RequestBody Book book) {
+        return bookService.createBook(book);
+    }
+
+    @PutMapping("/{id}")
+    public Book updateBook(@PathVariable String id, @RequestBody Book book) {
+        return bookService.updateBook(id, book);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable String id) {
+        bookService.deleteBook(id);
+    }
+}
