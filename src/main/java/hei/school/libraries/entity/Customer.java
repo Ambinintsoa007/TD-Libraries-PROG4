50a41e90652633045c6ec016f411f9dd3d597fe9
narindra20 <hei.narindra@gmail.com>
2026-06-09 10:50:55 +0300

    feat(entity): add customer entity

--- src/main/java/hei/school/libraries/entity/Customer.java
+++ src/main/java/hei/school/libraries/entity/Customer.java
@@ -1,6 +1,8 @@
 package hei.school.libraries.entity;
 
 import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,17 +20,16 @@ public class Customer {
   @GeneratedValue(strategy = GenerationType.UUID)
   private String id;
 
+  @Column(name = "first_name")
   private String firstName;
+
+  @Column(name = "last_name")
   private String lastName;
+
   private String address;
   private String phone;
-  private String notes;
+  private String note;
 
-  @ManyToOne
-  @JoinColumn(name = "library_id")
-  private Library library;
-
-  @OneToOne
-  @JoinColumn(name = "user_id")
-  private User user;
+  @OneToMany(mappedBy = "customer")
+  private List<Sale> sales = new ArrayList<>();
 }
