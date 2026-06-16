9bff05fcc36b933562d82611cb0188e739635f2b
Ambinintsoa007 <hei.ambinintsoa@gmail.com>
2026-06-16 11:19:50 +0300

    fix: return book DTOs to avoid lazy loading error

---
+++ src/main/java/hei/school/libraries/Dto/BookResponse.java
@@ -0,0 +1,13 @@
+package hei.school.libraries.Dto;
+
+
+import java.time.LocalDate;
+
+public record BookResponse(
+        String id,
+        String title,
+        String language,
+        String description,
+        String coverUrl,
+        LocalDate publicationDate
+) {}
