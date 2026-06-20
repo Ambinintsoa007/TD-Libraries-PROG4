99e62f64a5890f53e1db2fbaa471cedc7828ce1d
narindra20 <hei.narindra@gmail.com>
2026-06-20 21:04:13 +0300

    Add getLowStock to list books with low stock

--- src/main/java/hei/school/libraries/repository/BookCopyRepository.java
+++ src/main/java/hei/school/libraries/repository/BookCopyRepository.java
@@ -1,8 +1,15 @@
 package hei.school.libraries.repository;
 
 import hei.school.libraries.entity.BookCopy;
+import hei.school.libraries.entity.enums.Status;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BookCopyRepository extends JpaRepository<BookCopy, String> {}
+public interface BookCopyRepository extends JpaRepository<BookCopy, String> {
+
+  List<BookCopy> findByBook_IdAndStatus(String bookId, Status status);
+
+  long countByBook_IdAndStatus(String bookId, Status status);
+}
