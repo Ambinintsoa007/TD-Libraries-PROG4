14cd8c96a72229adac7386ab586e112f697dd785
narindra20 <hei.narindra@gmail.com>
2026-07-04 19:19:47 +0300

    feat: add SaleController with POST /sales endpoint

--- src/main/java/hei/school/libraries/endpoint/rest/controller/SaleController.java
+++ src/main/java/hei/school/libraries/endpoint/rest/controller/SaleController.java
@@ -1,4 +1,23 @@
 package hei.school.libraries.endpoint.rest.controller;
 
+import hei.school.libraries.Dto.SaleItemRequest;
+import hei.school.libraries.entity.Sale;
+import hei.school.libraries.service.SaleService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/sales")
+@RequiredArgsConstructor
 public class SaleController {
+
+  private final SaleService saleService;
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public Sale createSale(@RequestBody List<SaleItemRequest> lines) {
+    return saleService.createSale(lines);
+  }
 }
