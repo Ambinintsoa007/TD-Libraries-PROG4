1f32ae099b6d200916597cee0a4848cea6387af1
shaim <hei.shaim@gmail.com>
2026-07-07 09:41:48 +0100

    feat(service): add  BookCopyService

---
+++ src/main/java/hei/school/libraries/service/BookCopyService.java
@@ -0,0 +1,26 @@
+package hei.school.libraries.service;
+
+import hei.school.libraries.entity.BookCopy;
+import hei.school.libraries.entity.enums.Status;
+import hei.school.libraries.repository.BookCopyRepository;
+import hei.school.libraries.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+class BookCopyService {
+    private final BookRepository bookRepository;
+    private final BookCopyRepository bookCopyRepository;
+
+    public List<BookCopy> getCopies(String bookId, Status status) {
+        bookRepository
+                .findById(bookId)
+                .orElseThrow(() -> new RuntimeException("Book not found : " + bookId));
+
+        if (status != null) {
+            return bookCopyRepository.findByBook_IdAndStatus(bookId, status);
+        }
+        return bookCopyRepository.findByBook_Id(bookId);
+    }
+}
