8608cbc68b8cd462a92d08e8954608430455b8c2
shaim <hei.shaim@gmail.com>
2026-07-09 03:20:06 +0100

    feat(Service): add method to return book stock response

--- src/main/java/hei/school/libraries/service/BookService.java
+++ src/main/java/hei/school/libraries/service/BookService.java
@@ -1,6 +1,7 @@
 package hei.school.libraries.service;
 
 import hei.school.libraries.Dto.BookResponse;
+import hei.school.libraries.Dto.BookStockResponse;
 import hei.school.libraries.entity.Author;
 import hei.school.libraries.entity.Book;
 import hei.school.libraries.entity.BookCopy;
@@ -114,6 +115,12 @@ public class BookService {
   public long getAvailableStock(String bookId) {
     return bookCopyRepository.countByBook_IdAndStatus(bookId, Status.AVAILABLE);
   }
+
+    public BookStockResponse getBookStock(String bookId) {
+        Book book = getBookById(bookId);
+        long availableStock = getAvailableStock(bookId);
+        return new BookStockResponse(book.getId(), book.getTitle(), availableStock);
+    }
 
   @Transactional
   public Sale sell(String bookId, String customerId, int quantity) {
