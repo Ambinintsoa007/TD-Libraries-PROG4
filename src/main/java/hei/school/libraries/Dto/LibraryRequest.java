69025691e431390f0e0656b41adad347ebb7ca47
Ambinintsoa007 <hei.ambinintsoa@gmail.com>
2026-07-10 00:57:52 +0300

    feat: add library CRUD

---
+++ src/main/java/hei/school/libraries/Dto/LibraryRequest.java
@@ -0,0 +1,3 @@
+package hei.school.libraries.Dto;
+
+public record LibraryRequest(String name, String address, String phone) {}
---
+++ src/main/java/hei/school/libraries/Dto/LibraryResponse.java
@@ -0,0 +1,3 @@
+package hei.school.libraries.Dto;
+
+public record LibraryResponse(String id, String name, String address, String phone) {}
